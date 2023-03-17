5c6a36ecceae42fabb447ec8337f60e61c36bb36
jpromanonet <programming@jpromano.net>
2023-03-16 22:29:58 -0300

    Working on the starting point of the clone

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>I'm the Header</div>
-
-    {/* Left */}
+    <>
     
-  )
+        <div>I'm the Header</div>
+        
+        {/* Left */}
+        <div></div>
+    
+    </>
+  );
 }
 
 export default Header
